16d5f9f594118a615bcedb3a9864c48d6eacd15f
VishnuNadella <63490737+VishnuNadella@users.noreply.github.com>
2022-11-24 09:58:17 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from pymongo import *
+
+usn = "tle_hack_admin"
+pwd = "tle_hack_ffs007"
+connection = f"mongodb+srv://{usn}:{pwd}@cluster0.vzd4elr.mongodb.net/?retryWrites=true&w=majority"
+cluster = MongoClient(connection)
+
+db = cluster["hackathon"]
+collection = db["participants"]
+
+st.title("Welcome to TLE hack")
+st.subheader('Mark your self as attended')
+
+# df = pd.read_csv("./registered.csv")
+cursor = collection.find({})
+names = []
+for i in cursor:
+    # print(i["Name"])
+    names.append(i["Name"])
+    
+names = tuple(names)
+# df.drop()
+# name = tuple(df["Name"])
+
+person = st.selectbox(
+    "Please select your name",
+    names
+)
+
+if st.button("Mark as Attended"):
+    dets = collection.find_one({"Name" : person})
+    if dets["Attended?"] == False:
+        collection.update_one({"Name" : person}, {"$set" : {"Attended?" : True}})
+        st.success("Welcome to TLE hack, We wish you all the best")
+    elif dets["Attended?"] == True:
+        st.error("Please, you have already participated in hackathon")
+    
+    # if df.at[index, "Attended?"] == False:
+    #     stat = df.loc[index]["Attended?"]
+    #     df.at[index, "Attended?"] = True
+    #     # df.to_csv("./registered.csv", index = False)
+    #     st.success("Welcome to TLE hack, We wish you all the best")
+    # elif df.at[index, "Attended?"] == True:
+    #     st.error("Please, you have already participated in hackathon")
+
